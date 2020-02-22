e01b4a9ffb99f8a1698e18cb7f1e6db8f649ad05
jshoe54 <60626993+jshoe54@users.noreply.github.com>
2020-02-21 23:09:24 -0700

    Add files via upload
    i made the methods and added the elements, i still need to do the recursive method.

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,4 +1,82 @@
 
 public class SnakeGame {
-
+	private boolean gameboard[][];
+	private int position[];
+	
+	public SnakeGame() {
+		this.gameboard = new boolean[1][1];
+	}
+	
+	public SnakeGame(boolean[][]gameboard,int x,int y) {
+		this.gameboard=gameboard;
+		position[0]=x;
+		position[1]=y;
+		
+	}
+	
+	
+	private static int exhaustiveChecks;
+	
+	private static int recursiveChecks;
+	
+	public int[] findTailExhaustive(int x, int y) {
+		int neighbors=0;
+		int [] results = {0,0,0};
+		for(int i=0; i<gameboard.length;i++) {
+			for(int j=0; j<gameboard.length;j++) {
+				neighbors=0;
+				if(gameboard[i][j]) {
+					results[2]++;
+					if((i-1)>=0&&(j-1)>=0&&gameboard[i-1][j-1]){
+					neighbors++;
+					}
+					if((i-1)>=0&&gameboard[i-1][j]){
+					neighbors++;
+					}
+					if((j-1)>=0&&gameboard[i][j-1]){
+					neighbors++;
+					}
+					if((i+1)<gameboard.length&&(j+1)<gameboard.length&&gameboard[i+1][j+1]){
+					neighbors++;
+					}
+					if((i+1)<gameboard.length&&gameboard[i+1][j]){
+					neighbors++;
+					}
+					if((i+1)<gameboard.length&&gameboard[i][j+1]){
+					neighbors++;
+					}
+					if((i-1)>=0&&(j+1)<gameboard.length&&gameboard[i-1][j+1]){
+					neighbors++;
+					}
+					if((i+1)<gameboard.length&&(j-1)>=0&&gameboard[i+1][j-1]){
+					neighbors++;
+						}
+				}
+		if((neighbors==1)&&(!(position[0]==i)&&(position[1]==j))){
+			results[0]=i;
+			results[1]=j;
+			}
+		}
+			}
+		return results;
+	}
+	public int[] findTailRecursive() {
+		
+	}
+	
+	private int[] findTailRecursive(int[] currentPosition, int[] previousPosition) {
+		
+	}
+	
+	private void resetCounters() {
+		
+	}
+	
+	private static int getRecursiveChecks() {
+		
+	}
+	
+	private static int getExhaustiveChecks() {
+		
+	}
 }
